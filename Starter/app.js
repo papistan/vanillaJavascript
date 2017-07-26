d2d8d763a6bebf4e5c0936c48097ab9d8362f57b
papistan <jpapisan@gmail.com>
2017-07-26 11:56:12 -0700

    dot operator

--- Starter/app.js
+++ Starter/app.js
@@ -12,4 +12,69 @@
 
 // a()
 
-console.log(1 + '3')
+// console.log(1 + '3')
+
+// console.log(1 < 2 < 1)
+
+// console.log(Number('three'))
+
+// console.log('2' == 2)
+
+// console.log('2' === 2)
+
+// var a = 0
+
+// if (a) {
+// 	console.log('something is there')
+// }
+
+
+// var a = 1
+
+// if (a) {
+// 	console.log('something is there')
+// }
+
+// var a = 0
+
+// if (a || a === 0) {
+// 	console.log('something is there')
+// }
+
+// Default Values 
+
+// function greet(name) {
+// 	name = name || '<Your name here>'
+// 	console.log('Hello ' + name)
+// }
+
+ // es6 Default 
+// function greet(name = '<Your name here>') {
+// 	console.log('Hello ' + name)
+// }
+
+// greet()
+
+// Objects and functions
+
+var person = new Object()
+
+// person["firstName"] = "Tony"
+// person["lastName"] = "Alicia"
+
+// var firstNameProperty = "firstName"
+
+// console.log(person.firstName)
+// console.log(person[firstNameProperty])
+
+// Dot operator 
+
+person.city = new Object()
+// person.city = "Berkeley"
+person.city.zip = "zip"
+person.street = "Carleton"
+console.log(person.city.zip)
+
+
+
+
